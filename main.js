f11207f5f3ec86f6b2011e8419aa339ab707ac92
Gabriel Castro <gabriel.castro@itai.org.br>
2022-05-18 21:48:26 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+//mapping DOM elements
+var input = document.querySelector('#input');
+var btn = document.querySelector('#btnAdd');
+var taskList = document.querySelector('#taskList');
+
+//adding event to the button
+btn.addEventListener('click', function createTask(event){
+    var newTask = document.createElement('li');
+    newTask.setAttribute('class', 'task');
+
+    addTask(newTask);
+});
+
+function addTask(newTask){
+    //task validation
+    if(input.value.length == 0){
+        alert('Empty task, please add some content');
+    } else {
+        newTask.innerText = input.value;
+        taskList.append(newTask);
+    }
+
+    return newTask;
+}
+
