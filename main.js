d4501b37183b827c51ac216c3be43dd801dc1775
Gabriel Castro <gabriel.castro@itai.org.br>
2022-05-30 14:23:16 -0300

    New checkbox style

--- main.js
+++ main.js
@@ -8,37 +8,8 @@ btn.addEventListener('click', function createTask(event){
     var newTask = document.createElement('li');
     newTask.setAttribute('class', 'task');
     
-    validateTask(newTask);
+    addTask(newTask);
 });
-
-function validateTask(newTask){
-    //getting all the tasks to a NodeList
-    tasks = document.querySelectorAll('.task');
-
-    if (tasks.length == 0 && !input.value.length == 0) {
-        addTask(newTask)
-    } else  if(input.value.length == 0){
-        alert('Empty task, please fill with content');
-    } else {
-        //validation loop
-        for(var i = 0; i < tasks.length; i++){
-            if(newTask.innerHTML == tasks.item(i).innerHTML){
-                removeDuplicate(newTask);
-                if(confirm('duplicate task, add it anyway?')){
-                    addTask(newTask);
-                }
-                break; //Avoid repeating the same confirmation message
-            } else  if(input.value.length == 0){
-                alert('Empty task, please fill with content');
-                break;//Avoid repeating alert message
-            } else {
-                addTask(newTask);
-            }
-        }
-    }
-
-    return newTask;
-}
 
 function appendBtns(newTask){
     //Creating a div for the elements
@@ -70,8 +41,18 @@ function addTask(newTask){
     newTask.innerHTML = '<p>' + input.value + '</p>';
     appendBtns(newTask);
     taskList.append(newTask);
+    check(newTask);
 }
 
-function removeDuplicate(newTask){
-    newTask.remove();
+function check(newTask){//Only works for the first task element unforntully
+    var cbs = document.querySelector('.checkbox');
+
+    cbs.addEventListener('click', function isChecked(event){
+        if(cbs.checked == true){
+            cbs.parentElement.parentElement.setAttribute('class', 'task isChecked');
+        }else{
+            cbs.parentElement.parentElement.setAttribute('class', 'task');
+        }
+    })
+    
 }
