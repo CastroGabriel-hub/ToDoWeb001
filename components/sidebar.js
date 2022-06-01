e836d433bb42d2857a1ab115a8199e1ea46ebe60
Gabriel Castro <gabriel.castro@itai.org.br>
2022-06-01 17:27:01 -0300

    Message(javascript sidebar)

---
+++ components/sidebar.js
@@ -0,0 +1,36 @@
+const screen = document.querySelector('.screen');
+
+window.addEventListener('load', createSidebar);
+
+function createSidebar(e){
+    console.log('hey hoe')
+    //creating elements
+    const aside = document.createElement('aside');
+    aside.setAttribute('class', 'sidebar');
+
+    const nav = document.createElement('nav');
+    nav.setAttribute('class', 'menu');
+
+    const item1 = document.createElement('a');
+    item1.setAttribute('href', '#');
+    item1.setAttribute('class', 'menu-item selected');
+    item1.innerText = 'Tasks';
+    
+    const item2 = document.createElement('a');
+    item2.setAttribute('href', '#');
+    item2.setAttribute('class', 'menu-item');
+    item2.innerText = 'Profile';
+
+    const item3 = document.createElement('a');
+    item3.setAttribute('href', '#');
+    item3.setAttribute('class', 'menu-item');
+    item3.innerText = 'Statistics';
+
+    //appending elements
+    nav.appendChild(item1);
+    nav.appendChild(item2);
+    nav.appendChild(item3);
+    aside.appendChild(nav);
+    screen.insertBefore(aside, screen.firstChild);
+
+}
